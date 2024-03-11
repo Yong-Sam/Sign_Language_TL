6f857951d3499fe0e075fd7e45511131a6bbbad7
Subin <80036437+hansb1023@users.noreply.github.com>
2024-03-11 15:07:07 +0900

    [feat] Update handTracking.py

--- HandTracking/handTracking.py
+++ HandTracking/handTracking.py
@@ -1,47 +1,34 @@
-import cv2
 from cvzone.HandTrackingModule import HandDetector
+import cv2
 import socket
 
-# Parameters
-width,height = 1280, 720
-
-# IP WebCam
 cap = cv2.VideoCapture(0)
+cap.set(3, 1280)
+cap.set(4, 720)
+success, img = cap.read()
+h, w, _ = img.shape
+detector = HandDetector(detectionCon=0.8, maxHands=2)
 
-cap.set(3, width)
-cap.set(4, height)
-
-# 손 감지
-detector = HandDetector(maxHands=2, detectionCon=0.8)
-
-# 네트워크
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-serverAddressPort=("127.0.0.1", 5052)
+serverAddressPort = ("127.0.0.1", 5052)
 
 while True:
-    # 웹 캠에서 프레임 가져오기
+    # Get image frame
     success, img = cap.read()
-    # Hands
-    hands, img = detector.findHands(img)
-
+    # Find the hand and its landmarks
+    hands, img = detector.findHands(img)  # with draw
+    # hands = detector.findHands(img, draw=False)  # without draw
     data = []
 
-    # 21개의 랜드마크 값들을 UDP 프로토콜을 사용하여 Unity에 보냄
-    # Landmark values - (x,y,z)*21
-
     if hands:
-        # Get the first hand detected
+        # Hand 1
         hand = hands[0]
-        # Get the landmark list
-        lmList = hand['lmList']
-        print(lmList)
+        lmList = hand["lmList"]  # List of 21 Landmark points
         for lm in lmList:
-            data.extend([lm[0],height-lm[1],lm[2]])
-            print(data)
-            sock.sendto(str.encode(str(data)),serverAddressPort )
+            data.extend([lm[0], h - lm[1], lm[2]])
 
-            img = cv2.resize(img, (0,0), None, 0.5, 0.5)
-            cv2.imshow("Image",img)
+        sock.sendto(str.encode(str(data)), serverAddressPort)
 
-            if cv2.waitKey(1) == ord("q"):
-                break
+    # Display
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
