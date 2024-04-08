040c59466864246e47551b26928027aac25f4852
Subin <80036437+hansb1023@users.noreply.github.com>
2024-04-08 14:37:53 +0900

    [refactor] 포트번호 변경

--- Sign_Language_AR/Assets/03. Scripts/PythonConnect.cs
+++ Sign_Language_AR/Assets/03. Scripts/PythonConnect.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using System.Net;
 using System.Net.Sockets;
@@ -6,27 +9,33 @@ using UnityEngine.UI;
 
 public class PythonConnect : MonoBehaviour
 {
-    public string serverAddress = "192.168.0.27"; // Python 서버 IP 주소
-    public int serverPort = 5052; // Python 서버 포트 번호
+    public string serverAddress; // Python 서버 IP 주소
+    public int serverPort = 50000; // Python 서버 포트 번호
     public RawImage displayRenderer; // 텍스처를 출력할 오브젝트의 렌더러
 
     private Texture2D receivedTexture;
     private TcpClient client;
     private Thread receiveThread;
+    
+    private object lockObject = new object();
 
     void Start()
     {
+        Debug.Log("Start시작");
         ConnectToServer();
         receiveThread = new Thread(new ThreadStart(ReceiveData));
         receiveThread.Start();
+        Debug.Log("Start클래스 통과");
     }
 
     void ConnectToServer()
     {
+        Debug.Log("ConnectToServer시작");
         client = new TcpClient(serverAddress, serverPort);
+        Debug.Log("서버연결클래스 통과");
     }
 
-    void ReceiveData()
+    /*void ReceiveData()
     {
         byte[] buffer = new byte[4096];
         while (true)
@@ -42,15 +51,69 @@ public class PythonConnect : MonoBehaviour
                 }
             }
         }
-    }
+    }*/
+    void ReceiveData()
+    {
+        Debug.Log("ReceiveData시작");
+        try
+        {
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[4096];
+            List<byte> receivedBytes = new List<byte>();
+            byte[] jpegData = receivedBytes.ToArray();
 
+            while (true)
+            {
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    // 연결이 끊어진 경우
+                    break;
+                }
+
+                // 받은 데이터를 리스트에 추가
+                receivedBytes.AddRange(buffer.Take(bytesRead));
+
+                // 받은 데이터 처리
+                receivedTexture = new Texture2D(2, 2);
+                receivedTexture.LoadImage(jpegData);
+                receivedTexture.Reinitialize(receivedTexture.width, receivedTexture.height);
+            }
+            
+            lock (lockObject)
+            {
+                receivedTexture = new Texture2D(2, 2);
+                receivedTexture.LoadImage(jpegData);
+                receivedTexture.Reinitialize(receivedTexture.width, receivedTexture.height);
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"Error receiving data: {ex.Message}");
+        }
+        finally
+        {
+            client.Close();
+        }
+        Debug.Log("Receive Data클래스 통과");
+    }
+    
     void Update()
     {
+        Debug.Log("Update시작");
         // 받은 텍스처를 렌더러에 적용하여 출력
         if (receivedTexture != null)
         {
             displayRenderer.texture = receivedTexture;
         }
+        lock (lockObject)
+        {
+            if (receivedTexture != null)
+            {
+                displayRenderer.texture = receivedTexture;
+            }
+        }
+        Debug.Log("Update클래스 통과");
     }
 
     void OnDestroy()
@@ -63,5 +126,6 @@ public class PythonConnect : MonoBehaviour
         {
             receiveThread.Abort();
         }
+        Debug.Log("OnDestroy클래스 통과");
     }
 }
